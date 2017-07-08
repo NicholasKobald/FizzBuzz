dcaeb8e7b5a0c233dbcde21cf77521a676f9f3b9
Nicholas Kobald <nick.kobald@gmail.com>
2017-07-07 17:53:14 -0700

    now prints digits on their own line

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,37 +1,28 @@
 /*
-Nicholas Kobald 2016-04-09
-FizzBuzz with no control structures or loops
-*/
-public class FizzBuzz{
+ *FizzBuzz with no control structures or loops
+ *
+ * Nicholas Kobald 2016-04-09
+ * Modified 2017-07-07
+ */
+public class FizzBuzz {
+
 	private static final int ZERO = 0;
 	private static final int MAX_INT = 101;
 	private static final int ONE = 1;
-	
-	/*   
-		~~~~			______________
-			 /. .\  \\/~~~~~| Buzzzzzzzzzz |
-			| _  | //      \_____________/
-			 \  / //
-			 _||_//
-			//||
-			||||
-			 //\\
-			|| ||
-			/_  _\
-	*/ 
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		int Buzzer = 0;
-		System.out.print(Buzzer++);
+		System.out.println(Buzzer++);
 		DoFizzBuzz(Buzzer);
 	}
-	public static void DoFizzBuzz(int Buzzer){
+
+	public static void DoFizzBuzz(int Buzzer) {
 		int DivFive = Buzzer%5;
 		int DivThree = Buzzer%3;
 		int Helper = 1337;
-		try{
+		try {
 			Helper = (Helper)/(MAX_INT-Buzzer);
 		} catch(ArithmeticException e) {
-			System.out.println("We're done.");
 			System.exit(-1);
 		}
 		try{
@@ -44,25 +35,28 @@ public class FizzBuzz{
 		} catch(ArithmeticException e) {
 			MoreFizzBuzz(Buzzer);
 		}
-		System.out.print(" " + Buzzer + " ");
+		System.out.println(Buzzer);
 		Helper = 1337;
 		DoFizzBuzz(++Buzzer);
-		
+
 	}
-	public static void DoEvenMoreFizzBuzz(int Buzzer){
-		System.out.print(" Fizz ");
+
+	public static void DoEvenMoreFizzBuzz(int Buzzer) {
+		System.out.println("Fizz");
 		DoFizzBuzz(++Buzzer);
 	}
+
 	public static void MoreFizzBuzz(int Buzzer) {
 		int Helper = 1337;
 		int DivThree = Buzzer%3;
-		try{
+		
+		try {
 			Helper = Helper/DivThree;
 		} catch(ArithmeticException e) {
-			System.out.print(" FizzBuzz ");
+			System.out.println("FizzBuzz");
 			DoFizzBuzz(++Buzzer);
 		}
-		System.out.print(" Buzz " ); 
+		System.out.println("Buzz");
 		DoFizzBuzz(++Buzzer);
 	}
 }
